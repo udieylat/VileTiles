b5d4fe95a6a99b986fecf5f25ae447d2f0fba0ec
Udi Eylat <udi@ridewithvia.com>
2023-11-24 17:44:58 +0200

    more enemy attacks

---
+++ src/enemy_attacks/num_different_colors.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+from src.enemy_attacks.enemy_attack import EnemyAttack
+from src.models.enemy import Enemy
+
+
+class MaxSimilarColors(EnemyAttack):
+
+    def attack_for(
+            self,
+            enemy: Enemy,
+    ) -> int:
+        c = Counter([
+            tile.color.name
+            for tile in enemy.tiles
+        ])
+        return len(c)

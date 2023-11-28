9e64de9c749cad8dde429428dae830ccf41a0eca
Udi Eylat <udi@ridewithvia.com>
2023-11-28 18:01:03 +0200

    RectSameColorTiles

---
+++ src/elimination_conditions/rect_same_color_tiles.py
@@ -0,0 +1,24 @@
+from src.elimination_conditions.elimination_condition import IndependentEliminationCondition
+from src.models.enemy import Enemy
+
+
+class RectSameColorTiles(IndependentEliminationCondition):
+
+    @classmethod
+    def _eliminate_enemy(
+            cls,
+            enemy: Enemy,
+    ) -> bool:
+        rect_possible_indices = [
+            [0, 1, 3, 4, 6, 7],
+            [1, 2, 4, 5, 7, 8],
+            [0, 1, 2, 3, 4, 5],
+            [3, 4, 5, 6, 7, 8],
+        ]
+        return any(
+            all(
+                enemy[indices[0]].color == enemy[index].color
+                for index in indices
+            )
+            for indices in rect_possible_indices
+        )
